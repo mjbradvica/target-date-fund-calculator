9d7e6514c13c288666beed062adf73e852f74e20
Michael Bradvica <mjbradvica@protonmail.com>
2022-02-05 18:00:18 -0500

    charts and starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { CalculatorFormComponent } from 'src/features/calculator-form/calculator.form.component';
+import { CalculatorChartComponent } from 'src/features/chart/calculator.chart.component';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+	{
+		path: 'calculator',
+		component: CalculatorFormComponent,
+	},
+	{
+		path: 'result',
+		component: CalculatorChartComponent,
+	},
+	{
+		path: '',
+		redirectTo: '/calculator',
+		pathMatch: 'full',
+	},
+];
+
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule],
+	declarations: [AppComponent, CalculatorFormComponent, CalculatorChartComponent],
+	imports: [BrowserModule, RouterModule.forRoot(routes)],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ src/features/calculator-form/calculator.form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'calculator-form',
+	templateUrl: 'calculator.form.component.html',
+})
+export class CalculatorFormComponent {
+	constructor() {}
+}
